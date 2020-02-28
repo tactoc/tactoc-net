f1888231c35fd9b660e681617ee664aa6fc2e521
viktorholk <viktorholk@gmail.com>
2020-02-28 17:10:38 +0100

    .

--- main.py
+++ main.py
@@ -216,12 +216,11 @@ class Cloud(object):
         with zipfile.ZipFile(memory_zip, "w", zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk(path):
                 print(root)
-                dir_path = root.split(self.username + "/")[1]
+                dir_path = root.split(self.username + "/")
                 print(dir_path)
 
-            for file in files:
-			print(f"WRITE FROM {os.path.join(root,file)} TO: {os.path.join(dir_path,file)}")
-                        zipf.write(os.path.join(root,file), os.path.join(dir_path, file))
+            # for file in files:
+            #     zipf.write(os.path.join(root,file), os.path.join(dir_path, file))
 
         memory_zip.seek(0)
         return memory_zip
