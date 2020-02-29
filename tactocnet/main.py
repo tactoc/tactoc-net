b4df1569e8119a84c10776b3501bb8a8e24722f7
viktorholk <viktorholk@gmail.com>
2020-02-29 21:34:58 +0100

    Fixed list unpack for python 2.7

--- tactocnet/main.py
+++ tactocnet/main.py
@@ -1,5 +1,6 @@
 from flask import Flask,Markup,Blueprint, render_template, session, current_app, send_file ,send_from_directory, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user, logout_user
+from __future__ import print_function
 import os
 import shutil
 import zipfile
