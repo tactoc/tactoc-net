4b38c5d7621fc15e190d27a86fb61d82050da3f7
viktorholk <viktorholk@gmail.com>
2020-03-03 07:05:05 +0000

    Run ip to 180

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,4 @@
 #instance config.py
-
 SECRET_KEY                      = 'MZvKzJmUl8gN6z5jptAO'
 SQLALCHEMY_DATABASE_URI         = 'sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS  = True
--- run.py
+++ run.py
@@ -5,16 +5,16 @@ from sys import argv
 if __name__ == "__main__":
     args = {}
     args["debug"]           = False
-    args["host"]            = "0.0.0.0"
+    args["host"]            = "192.168.10.180"
     args["port"]            = 80
     args["threaded"]        = True
+
+    if  "-ssl" in argv:
+        args["port"]       = 443
 
     if  "-l" in argv:
         args["host"]        = "localhost"
     if "-d" in argv:
         args["debug"]       = True
 
-
-
-    
     app.run(**args)
