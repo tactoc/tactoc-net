bcc93aa61f25b07131080ce900128974830fe887
viktorholk <viktorholk@gmail.com>
2020-03-06 23:26:12 +0100

    DEBUG PRINT = FALSE, max content size = 100 gb

--- instance/config.py
+++ instance/config.py
@@ -2,7 +2,7 @@
 SECRET_KEY                      = 'MZvKzJmUl8gN6z5jptAO'
 SQLALCHEMY_DATABASE_URI         = 'sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS  = True
-MAX_CONTENT_LENGTH              = 10 * 1024 * 1024 * 1024 # 10 gb max
+MAX_CONTENT_LENGTH              = 100 * 1024 * 1024 * 1024 # 10 gb max
 
 MEDIA_FOLDER                    = '/media'
 TACTOCNET_FOLDER                = '/media/tactocnet'
@@ -10,5 +10,5 @@ UPLOADS_FOLDER                  = '/media/tactocnet/uploads'
 CLOUD_FOLDER                    = '/media/tactocnet/cloud'
 
 #Debugging
-DEBUG_PRINT                     = True
+DEBUG_PRINT                     = False
 
